105925b29d11ed695c67916333b4d39e8349a7c2
Tong <200839284@qq.com>
2025-09-14 16:18:09 +1200

    browser context

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -1,6 +1,9 @@
 const {test} = require('@playwright/test');
 
-test('First Playwright Test', async function () 
+test('First Playwright Test', async ({browser}) =>
 {
-  // Test code goes here
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  await page.goto('https://orange-plant-0ccfa0d00.1.azurestaticapps.net/');
+
 });
