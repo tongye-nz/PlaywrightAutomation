f19fb8063e6af552cf442d32c8ff05aa725ff963
Tong <200839284@qq.com>
2025-09-25 23:42:20 +1200

    use locator to automation typing input

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -4,9 +4,12 @@ test.only('First Playwright Test', async ({browser}) =>
 {
   const context = await browser.newContext();
   const page = await context.newPage();
-  await page.goto('https://orange-plant-0ccfa0d00.1.azurestaticapps.net/');
+  await page.goto('https://orange-plant-0ccfa0d00.1.azurestaticapps.net/login');
   console.log(await page.title());
   await expect(page).toHaveTitle(/PetWhenua/);
-
+  //CSS, Xpath
+  await page.locator('')
+  await page.locator('#«r4»').type('admin');
+  await page.locator('#«r5»').type('admin@pass');
 
 });
