5ca33a8856ac1911064efc9b40ad8cc7d6b30fbc
Tong <200839284@qq.com>
2025-10-17 16:02:39 +1300

    handle child window and tab

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -28,7 +28,7 @@ test('First Playwright Test', async ({browser}) =>
 
 });
 
-test.only('UI Controls', async ({page})=>
+test('UI Controls', async ({page})=>
 {
   await page.goto("https://rahulshettyacademy.com/loginpagePractise");
   const userName = page.locator('#username');
@@ -40,3 +40,28 @@ test.only('UI Controls', async ({page})=>
   await page.pause();
   
 });
+
+test.only('@Child windows hadl', async ({browser})=>
+ {
+    const context = await browser.newContext();
+    const page =  await context.newPage();
+    const userName = page.locator('#username');
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+    const documentLink = page.locator("[href*='documents-request']");
+ 
+    const [newPage]=await Promise.all(
+   [
+      context.waitForEvent('page'),//listen for any new page pending,rejected,fulfilled
+      documentLink.click(),
+   
+   ])//new page is opened
+   
+ 
+   const  text = await newPage.locator(".red").textContent();
+    const arrayText = text.split("@")
+    const domain =  arrayText[1].split(" ")[0]
+    //console.log(domain);
+    await page.locator("#username").fill(domain);
+    console.log(await page.locator("#username").textContent());
+ 
+ });
