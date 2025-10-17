0f6a7aa21798a1cd579ec8fae013016c47ed32d3
Tong <200839284@qq.com>
2025-10-17 14:14:26 +1300

    Create SelfUIBasicstest.spec.js

---
+++ tests/SelfUIBasicstest.spec.js
@@ -0,0 +1,33 @@
+const {test, expect} = require('@playwright/test');
+
+test('First Playwright Test', async ({browser}) =>
+  // test.only = only run this test script
+{
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  await page.goto('https://orange-plant-0ccfa0d00.1.azurestaticapps.net/login');
+  console.log(await page.title());
+  await expect(page).toHaveTitle(/PetWhenua/);
+
+  //CSS, Xpath
+  const userName = page.locator('#«r4»');
+  const password = page.locator('#«r5»');
+  const signIn = page.locator('main').getByRole('button', { name: 'Sign In' }); //MUI React 'Sign in' button
+
+  await page.locator('')
+  await userName.type('admi');
+  await password.type('admin@pass'); 
+  await signIn.click(); 
+  // if CSS: await page.locator('#buttonId').click();
+
+  // web driver wait
+  const errorMsg = await page.getByRole('alert').textContent(); //MUI React 'alert' error message
+  console.log('Error:', errorMsg.trim());
+  // if CSS: console.log(await page.locator("[style='block']").textContent());
+
+  //type fill
+  await userName.fill('');
+  await userName.fill('admin');
+  await signIn.click();
+
+});
