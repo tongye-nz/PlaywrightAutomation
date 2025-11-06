b8f9ec229ff54051b114eb855798ec358ad6274c
Tong <200839284@qq.com>
2025-11-06 16:48:19 +1300

    add auto register detail

---
+++ tests/my-msa-webapp-register.spec.js
@@ -0,0 +1,16 @@
+import { test, expect } from '@playwright/test';
+
+test('Playwright Special locators', async ({ page }) => {
+
+    await page.goto("https://orange-plant-0ccfa0d00.1.azurestaticapps.net/register");
+    await expect(page.getByText("Create Your Account")).toBeVisible();
+
+    await page.getByPlaceholder("First Name").fill("playwright");
+    await page.getByPlaceholder("Last Name").fill("test01");
+    await page.getByPlaceholder("Userame").fill("playwright_test01");
+    await page.getByPlaceholder("Email").fill("playwright_test01@test.com");
+    await page.getByPlaceholder("Password").fill("Pass@987");
+    await page.getByPlaceholder("Confirm Password").fill("Pass@987");
+    await page.getByRole("button",{name : "Create Account"}).click();
+
+});
