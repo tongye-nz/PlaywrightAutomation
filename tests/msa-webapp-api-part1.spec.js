b4772945d4eae5e0cbe44d94e620dff7a33f41ce
Tong <200839284@qq.com>
2025-11-09 18:32:43 +1300

    Update msa-webapp-api-part1.spec.js

--- tests/msa-webapp-api-part1.spec.js
+++ tests/msa-webapp-api-part1.spec.js
@@ -1,6 +1,6 @@
 const {test, expect, request} = require('@playwright/test');
 const loginPayload = {usernameOrEmail: "vet01", password: "TestPass123!"};
-
+let token;
 test.beforeAll( async ()=>
 {
     const apiContext = await request.newContext();
@@ -10,19 +10,22 @@ test.beforeAll( async ()=>
         }) // 200, 201
     expect(loginResponse.ok()).toBeTruthy();
     const loginResponseJson = await loginResponse.json();
-    const token = loginResponseJson.token;
+    token = loginResponseJson.token;
     console.log(token);
 
 });
 
 test('Playwright getBy with MSA login page', async ({ page }) => {
 
-    await page.goto("https://orange-plant-0ccfa0d00.1.azurestaticapps.net/login");
-    await expect(page.getByText("Sign in to PetWhenua")).toBeVisible();
+    page.addInitScript(value => {
+        window.localStorage.setItem("token", value);
+    }, token)
 
-    await page.getByPlaceholder("Email or Username").fill("vet01");
-    await page.getByPlaceholder('Enter your password').fill('TestPass123!');
-    await page.locator('main').getByRole('button', { name: 'Sign In' }).click();
-    await page.waitForTimeout(3000);
+    await page.goto("https://orange-plant-0ccfa0d00.1.azurestaticapps.net/profile");
+    // await page.goto("https://orange-plant-0ccfa0d00.1.azurestaticapps.net/login");
+    // await expect(page.getByText("Sign in to PetWhenua")).toBeVisible();
+    // await page.getByPlaceholder("Email or Username").fill("vet01");
+    // await page.getByPlaceholder('Enter your password').fill('TestPass123!');
+    // await page.locator('main').getByRole('button', { name: 'Sign In' }).click();
     await expect(page.getByText("My Profile")).toBeVisible();
 });
